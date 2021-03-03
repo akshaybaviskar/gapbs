1bfada99aa52ebfe11e372b814bb817a8c90cffe
akshaybaviskar <baviskar.akshay01@gmail.com>
2021-03-03 14:22:20 +0000

    Build commands to build all benchmarks as single threaded.

---
+++ build_st.sh
@@ -0,0 +1,8 @@
+g++ -std=c++11 -O3 -Wall  src/bc.cc -o bc_st
+g++ -std=c++11 -O3 -Wall  src/bfs.cc -o bfs_st
+g++ -std=c++11 -O3 -Wall  src/cc.cc -o cc_st
+g++ -std=c++11 -O3 -Wall  src/cc_sv.cc -o cc_sv_st
+g++ -std=c++11 -O3 -Wall  src/pr.cc -o pr_st
+g++ -std=c++11 -O3 -Wall  src/sssp.cc -o sssp_st
+g++ -std=c++11 -O3 -Wall  src/tc.cc -o tc_st
+g++ -std=c++11 -O3 -Wall  src/converter.cc -o converter_st
